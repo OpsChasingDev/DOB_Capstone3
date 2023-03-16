cfdaabdbb2fb22cf2303ea7fea12b68c14b0356e
Robert Stapleton <robert@stapleton-family.net>
2023-03-16 19:59:08 -0400

    modify entry script for docker-compose

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -3,4 +3,7 @@ sudo yum update -y && sudo yum install -y docker
 sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 sudo chmod 666 /var/run/docker.sock
-docker run -p 8080:80 nginx
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
