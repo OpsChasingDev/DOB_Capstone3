03c6dd155ad587e310584988f4d04f189a40035c
Robert Stapleton <robert@stapleton-family.net>
2023-03-16 20:04:08 -0400

    removed unneeded output from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,10 +73,6 @@ data "aws_ami" "latest-amazon-linux-image" {
   }
 }
 
-output "aws_ami_id" {
-  value = data.aws_ami.latest-amazon-linux-image.id
-}
-
 output "ec2_public_ip" {
   value = aws_instance.myapp-server.public_ip
 }
